2b25466c32d58b15fbd610b69af0646196bf93a5
luv-n-pce <loves2peace.1945@gmail.com>
2020-12-15 21:25:37 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root to: 'memories#index'
   devise_for :users
   resources :memories do
+    resources :comments, only: [:create, :destroy]
     resource :likes, only: [:create, :destroy]
     collection do
       get 'search'
     end
   end
-
   resources :users, only: :show do
     resources :plans
   end
