48c6795a22be2d590e331a5fbd55a6133cfd871e
luv-n-pce <loves2peace.1945@gmail.com>
2020-12-15 18:01:54 +0900

    new, createの作成

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -5,4 +5,24 @@ class PlansController < ApplicationController
 
   end
 
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to user_plans_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:country_id, :place, :text).merge(user_id: current_user.id)
+  end
+
+ 
 end
