dba9555efadaf9fd0427e77227f3ee70e9dd69de
luv-n-pce <loves2peace.1945@gmail.com>
2020-12-15 17:18:17 +0900

    モデルの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :memories, dependent: :destroy
+
   with_options presence: true do
     validates :nickname, length: { maximum: 40 }
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "includes both letters and numbers"}
