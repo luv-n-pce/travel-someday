ea8bd24d4c88efceac44dc5348cc597a7fb80ec2
luv-n-pce <loves2peace.1945@gmail.com>
2020-12-15 15:45:02 +0900

    モデルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, length: { maximum: 40 }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "includes both letters and numbers"}
+  end
 end
